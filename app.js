1571d1fb24ce73830aed651640719b25df9c0b76
JohnyDeCoder <johnymora251@gmail.com>
2023-02-09 08:08:37 -0600

    Added logGreeting function

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ function greet() {
 // function expression created on the fly
 let logGreeting = (miParametro) => miParametro();
 
+logGreeting(function () {
+    console.log('Hello from a function created on the fly');
+})
+
 logGreeting(greet);
