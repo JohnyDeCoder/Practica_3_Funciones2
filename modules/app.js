0203900f5dbf6ec517ea24510500aec7894aa1be
JohnyDeCoder <johnymora251@gmail.com>
2023-02-09 08:23:42 -0600

    Added modules folder and created greet.js

---
+++ modules/app.js
@@ -0,0 +1,20 @@
+require('./greet');
+
+function greet() {
+    console.log('Hello');
+}
+
+// function expression created on the fly
+let logGreeting = (miParametro) => miParametro();
+
+logGreeting(function () {
+    console.log('Hello from a function created on the fly');
+})
+
+// interpolación de strings "clásica"
+let logGreeting1 = (miNombre, miColor) => {
+    console.log("¡Hola " + miNombre + " buenos días! Tu color favorito es el " + miColor);
+}
+
+logGreeting(greet);
+logGreeting1("Johny", "Azul");
