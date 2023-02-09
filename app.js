7638292ce9f546b0292512ddfad6e729d3000c12
JohnyDeCoder <johnymora251@gmail.com>
2023-02-09 08:12:48 -0600

    Added string interpolation clasic

--- app.js
+++ app.js
@@ -9,4 +9,10 @@ logGreeting(function () {
     console.log('Hello from a function created on the fly');
 })
 
+// interpolación de strings "clásica"
+let logGreeting1 = (miNombre, miColor) => {
+    console.log("¡Hola " + miNombre + " buenos días! Tu color favorito es el " + miColor);
+}
+
 logGreeting(greet);
+logGreeting1("Johny", "Azul");
